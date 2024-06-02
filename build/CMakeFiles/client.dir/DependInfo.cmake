b13550b8917aba50ea9e32ab5221e01a71dd9195
osnatsho <149496035+osnatsho@users.noreply.github.com>
2024-06-02 09:31:58 +0300

    Add files via upload

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/ConnectionHandler.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/ConnectionHandler.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/MessageFactory.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/MessageFactory.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/ResponseManager.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/ResponseManager.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/StateControler.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/StateControler.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/StompClient.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/StompClient.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/User.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/User.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/event.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/event.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/keyboardInputSend.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/keyboardInputSend.cpp.o"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src/socketReader.cpp" "/home/spl211/Desktop/SPL231-Assignment3-template/build/CMakeFiles/client.dir/src/socketReader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/."
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/include"
+  "/home/spl211/Desktop/SPL231-Assignment3-template/SPL231-Assignment3-student-template/client/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/StompClient.cpp.o"
+  "CMakeFiles/client.dir/src/ConnectionHandler.cpp.o"
+  "CMakeFiles/client.dir/src/event.cpp.o"
+  "CMakeFiles/client.dir/src/keyboardInputSend.cpp.o"
+  "CMakeFiles/client.dir/src/MessageFactory.cpp.o"
+  "CMakeFiles/client.dir/src/ResponseManager.cpp.o"
+  "CMakeFiles/client.dir/src/socketReader.cpp.o"
+  "CMakeFiles/client.dir/src/StateControler.cpp.o"
+  "CMakeFiles/client.dir/src/User.cpp.o"
+  "client.pdb"
+  "client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
